fd28515a2a5fd785d14c7eee8c361286e2560e48
jerryjatweb <jerryjatweb@gmail.com>
2022-09-13 08:01:13 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int get_first(int);
+int get_last(int);
+void print_any_int(int);
+void print_times_table(int);
+long get_first_l(long);
