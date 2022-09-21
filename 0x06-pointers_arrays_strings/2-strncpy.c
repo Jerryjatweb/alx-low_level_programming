2b1ca87be9f0fefadea36a92f5847658e6497785
jerryjatweb <jerryjatweb@gmail.com>
2022-09-21 04:42:04 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies source string to destination, but will
+ *	      only copy up to N characters
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of bytes to be copied from src string
+ *
+ * Return: Pointer to destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count;
+
+	for (count = 0; count < n && src[count]; count++)
+	{
+		dest[count] = src[count];
+	}
+	for (; count < n; count++)
+		dest[count] = '\0';
+
+	return (dest);
+}
