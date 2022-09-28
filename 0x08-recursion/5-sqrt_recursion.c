85dbc791fbce562f4ffcd550eb53dbda74d0f24a
jerryjatweb <jerryjatweb@gmail.com>
2022-09-28 15:26:02 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - gets the square root of `n` using recursion
+ * @n: number to get square root of
+ *
+ * Return: square root of `n`
+ */
+int _sqrt_recursion(int n)
+{
+	return (check_square(0, n));
+}
+
+/**
+ * check_square - searches for the square root of n, recursively
+ * @i: square root to check first
+ * @n: number to find root of
+ *
+ * Return: Square root of number, -1 if no whole root
+ */
+int check_square(int i, int n)
+{
+	int sq = i * i;
+
+	if (sq > n)
+		return (-1);
+
+	if (sq == n)
+		return (i);
+
+	return (check_square(i + 1, n));
+}
