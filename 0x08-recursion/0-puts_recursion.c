34c537679acea8e0ee898669a26f18d377ad4a26
jerryjatweb <jerryjatweb@gmail.com>
2022-09-28 15:11:45 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - uses _putchar recursively to print a string
+ * @s: pointer to first char in string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int check_square(int, int);
+int is_prime_number(int n);
+int check_prime(int, int);
+int is_palindrome(char *s);
+int _check_palindrome(char *, int);
+int _getlen(char *, int);
+int wildcmp(char *, char *);
