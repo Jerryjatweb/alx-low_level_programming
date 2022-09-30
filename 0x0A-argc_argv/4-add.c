4419dd3d664c7869d86b27ff7a50e68f6c26ce16
jerryjatweb <jerryjatweb@gmail.com>
2022-09-30 15:31:20 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * tonum - Converts string to positive number
+ * @c: pointer to first character in string
+ *
+ * Return: Integer value of string
  */
-int main(int argc, char *argv[])
+int tonum(char *c)
 {
-	int i, sum = 0;
+	int num = 0;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	for (; *c; c++)
 	{
-		if (!atoi(argv[i]))
+		if (*c >= '0' && *c <= '9')
 		{
-			printf("%s\n", "Error");
+			num *= 10;
+			num += (*c - '0');
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+
+	return (num);
+}
+
+/**
+ * main - Prints the sum of all arguments
+ * @argc: number of arguments including command name
+ * @argv: Pointer to array of pointers to arguments as strings including
+ *			command name
+ *
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int res;
+
+	for (argv++, argc--; argc--; argv++)
+	{
+		res = tonum(*argv);
+		if (res < 0)
+		{
+			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+
+		sum += res;
 	}
+
 	printf("%d\n", sum);
 
 	return (0);
